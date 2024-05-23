2520c08fcbb560f69dd33d28cb14c06c35917c26
Ruslan <ruslanmarlinskiy@gmail.com>
2024-05-24 01:48:08 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        final int inch = 15;
+        System.out.println(inch);
+        System.out.println(Test.CONSTANT);
+    }
+}
+
+class Test {
+    public static final int CONSTANT = 10;
+}
